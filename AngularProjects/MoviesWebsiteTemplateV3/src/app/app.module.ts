6715023ddcce71f6a29e5aa0781a16fb1a53bcd9
BongaDigiLink <bonga@digilink.africa>
2023-04-25 14:48:08 +0200

    new project.

--- AngularProjects/MoviesWebsiteTemplateV3/src/app/app.module.ts
+++ AngularProjects/MoviesWebsiteTemplateV3/src/app/app.module.ts
@@ -1,30 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
-import { MovieComponent } from './movie/movie.component';
-import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
-//Add routes array hear
-const routes: Routes = [
-  { path:'', redirectTo: 'login', pathMatch:'full' },
-  { path: 'login', component: LoginComponent},
-  { path: 'home', component: HomeComponent },
-  { path: 'movie', component: MovieComponent },
-];
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HeaderComponent,
+    HomeComponent,
+    LoginComponent,
     MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
