c9099b1626a1b87a183ee4fd81519f0c3a5d9af0
BongaDigiLink <bonga@digilink.africa>
2023-04-25 10:37:47 +0200

    implementing Comparable class, comparing by string length and ( name.compareTo(person.name) )

--- NaturalOrdering/src/models/Person.java
+++ NaturalOrdering/src/models/Person.java
@@ -1,6 +1,8 @@
 package models;
 
-public class Person 
+import java.util.Objects;
+
+public class Person implements Comparable<Person>
 {
 	private String name;
 	
@@ -12,5 +14,46 @@ public class Person
 	public String toString()
 	{
 		return name;
+	}
+
+	@Override
+	public int compareTo(Person person) 
+	{
+		//return name.compareTo(person.name);
+		
+		//Sorting based on the length of the string.
+		int len2 = person.name.length();
+		int len1 = name.length();
+		
+		if(len1 > len2)
+		{
+			return 1;
+		}
+		
+		if(len2 > len1)
+		{
+			return -1;
+		}
+		
+		return 0;
+	}
+
+	@Override
+	public int hashCode() 
+	{
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(name, other.name);
 	}
 }
